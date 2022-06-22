78802082ad2025fb5ae0950125113df37aa55ce9
misky-jelle <miskyjelle@gmail.com>
2022-06-22 10:01:34 +0300

    first commit

---
+++ strings.py
@@ -0,0 +1,38 @@
+# def splitting():
+#  x="childrenplayinginplayground"
+# y=len(x)
+# firsthalf=x.lower()
+# half=x.upper()
+# split=y-(y//2)
+# user=half[:split]+firsthalf[split:]
+# return user
+
+# print(splitting())
+
+# x="childrenplayinginplayground"
+# y=len(x)//2
+# second=x[y:].upper()
+# first=x[:y].lower()
+# third=first+second
+# print(third)
+
+# n="childrenplayinginplayground"
+# m=len(n)//2
+# i=n[:m].lower()
+# u=n[m:].upper()
+# s=u+i
+# print(s)
+
+n="childrenplayinginplayground"
+k=len(n)//2
+y=n[k:].upper()
+j=n[:k].lower()
+# v=y+j
+print(j+y)
+# print(y)
+# print(j)
+
+
+
+
+
